1d312dca52d6c905aadb26a6a7f3c8ba538c1dc4
Shelarnitin <shelarnitin399@gmail.com>
2025-09-21 22:54:24 +0530

    DSA

--- Array/array2.js
+++ Array/array2.js
@@ -2,34 +2,34 @@
 
 // function findNumber(arr,target){
 //     for(let i=0;i<arr.length;i++){
-//         if(arr[i]===target){
-//             return true;
+//         if(arr[i] === target){
+//             return 'Number Exist'
 //         }
 //     }
-//     return false
+//     return 'Number dosen`t Exist'
 // }
-// console.log(findNumber([1,2,3,4,5],9));
-// console.log(findNumber([1,2,3,4,5],5));
+// console.log(findNumber([1,2,3,4,5],45))
 
 
 // Q print the first and last element of the array
 
 // function firstAndLast(arr){
-//     if(arr.length===0){
-//         console.log("array is empty")
-//         return
-//     }
+//     // if(arr.length===0){
+//     //     console.log("array is empty")
+//     //     return
+//     // }
 //     console.log("first element is",arr[0])
+//     console.log("Middle element is",arr[Math.floor(arr.length/2)])
 //     console.log("last element is",arr[arr.length-1])
 // }
-// firstAndLast([10,20,34])
+// firstAndLast([10,20,34,44,45])
 // firstAndLast([])
 
-//******************LINEAR AEARCH********************/
+//******************LINEAR SEARCH********************/
 // **********Find if an element exists***************
 
-// let arr = [10,20,30,40]
-// function linearSearch(arr,target){
+// let arr = [ 1,2,3,4,5,6]
+// function lenearsearch(arr,target){
 //     for(let i=0;i<arr.length;i++){
 //         if(arr[i]===target){
 //             return true
@@ -37,11 +37,24 @@
 //     }
 //     return false
 // }
-// console.log(linearSearch(arr,30))
-// console.log(linearSearch(arr,300))
+// console.log(lenearsearch(arr,3))
+// console.log(lenearsearch(arr,31))
 
 
 //************Binary search --> works only on sorted array******/
+// find element index
+// formula (1)
+// function findIndex(arr,target){
+//     for(let i=0;i<arr.length;i++){
+//         if(arr[i]===target){
+//             return arr[i] -1
+//         }
+//     }
+//     return false
+// }
+// console.log(findIndex([1,2,3,4,5],5))
+
+// formula (2)
 // function binarySearch(arr,target){
 //     let low = 0, high= arr.length-1
 //     while(low<=high){
@@ -52,7 +65,7 @@
 //     }
 //     return -1
 // }
-// console.log(binarySearch([1,2,3,4,5],5))
+// console.log(binarySearch([1,2,3,4,5],2))
 
 //***************Reversing --> Reverse the order of element****/
 // function reverseArr(arr){
@@ -67,15 +80,15 @@
 // console.log(reverseArr([1,2,3,4,5]))
 
 // ************Insert element at kth position*************
-function insert(arr,k,value){
-    arr.splice(k,0,value)
-    return arr
-}
-console.log(insert([1,2,3,4,5],2,200))
+// function insert(arr,k,value){
+//     arr.splice(k,0,value)
+//     return arr
+// }
+// console.log(insert([1,2,3,4,5],2,200))
 
 // *************Delete element at kth position************
-function deleteAt(arr,k){
-    arr.splice(k,1);
-    return arr;
-}
-console.log(deleteAt([10,20,30,40]))
+// function deleteAt(arr,k){
+//     arr.splice(k,1);
+//     return arr;
+// }
+// console.log(deleteAt([10,20,30,40]))
