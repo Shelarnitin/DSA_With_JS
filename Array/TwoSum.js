8d7a30f311e72c698133531f9f04a72ead48d58d
Shelarnitin <shelarnitin399@gmail.com>
2025-11-20 23:23:17 +0530

    longestcommonPrefix

--- Array/TwoSum.js
+++ Array/TwoSum.js
@@ -14,3 +14,25 @@ const nums = [2, 7, 11, 15];
 const target = 9;
 
 console.log(twoSum(nums, target));
+
+
+/***********
+Write a function to find the longest common prefix string amongst an array of strings.
+
+If there is no common prefix, return an empty string "".
+
+********** */
+
+var longestCommonPrefix = function(strs) {
+    if (!strs.length) return "";
+    strs.sort();
+    let first = strs[0], last = strs[strs.length - 1], result = "";
+    for (let i = 0; i < first.length; i++) {
+        if (first[i] === last[i]) {
+            result += first[i];
+        } else {
+            break;
+        }
+    }
+    return result;
+};
