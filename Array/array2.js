86b7f4680a0e7037b0d7c48d86c0a4e3b90667c9
Shelarnitin <shelarnitin399@gmail.com>
2026-02-21 11:45:03 +0530

    First and Last

--- Array/array2.js
+++ Array/array2.js
@@ -17,17 +17,18 @@ console.log(findNumber([1,2,3,4,5],55))
 
 // Q print the first and last element of the array
 
-// function firstAndLast(arr){
-//     // if(arr.length===0){
-//     //     console.log("array is empty")
-//     //     return
-//     // }
-//     console.log("first element is",arr[0])
-//     console.log("Middle element is",arr[Math.floor(arr.length/2)])
-//     console.log("last element is",arr[arr.length-1])
-// }
-// firstAndLast([10,20,34,44,45])
-// firstAndLast([])
+function firstAndLast(arr){
+    if(arr.length === 0){
+        console.log("Empty array")
+        return
+    }
+    console.log('First Element is = ',arr[0])
+    console.log('last Element is = ',arr[arr.length-1])
+}
+// console.log(firstAndLast([1,2,3,4,5]))
+// console.log(firstAndLast([]))
+
+
 
 //******************LINEAR SEARCH********************/
 // **********Find if an element exists***************
