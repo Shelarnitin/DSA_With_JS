df56965af5c2a864dc89fa7913d6cdd4998381d6
Shelarnitin <shelarnitin399@gmail.com>
2026-03-16 20:49:20 +0530

    Prefix

--- Array/TwoSum.js
+++ Array/TwoSum.js
@@ -23,16 +23,16 @@ If there is no common prefix, return an empty string "".
 
 ********** */
 
-var longestCommonPrefix = function(strs) {
-    if (!strs.length) return "";
-    strs.sort();
-    let first = strs[0], last = strs[strs.length - 1], result = "";
-    for (let i = 0; i < first.length; i++) {
-        if (first[i] === last[i]) {
-            result += first[i];
-        } else {
-            break;
-        }
-    }
-    return result;
-};
+// var longestCommonPrefix = function(strs) {
+//     if (!strs.length) return "";
+//     strs.sort();
+//     let first = strs[0], last = strs[strs.length - 1], result = "";
+//     for (let i = 0; i < first.length; i++) {
+//         if (first[i] === last[i]) {
+//             result += first[i];
+//         } else {
+//             break;
+//         }
+//     }
+//     return result;
+// };
